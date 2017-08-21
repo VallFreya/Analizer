292089c0ad6fc95530b7bf3d3b7c1b7627858f4a
Diana <dexpvallfreya@gmail.com>
2017-08-21 15:48:10 +0300

    Повышена версия Framework, упрощены методы проверки правила, убраны все ненужные переменные

--- Analizer/MainWindow.xaml.cs
+++ Analizer/MainWindow.xaml.cs
@@ -10,12 +10,10 @@ namespace Analizer
 {
     public partial class MainWindow
     {
-        readonly string[] _all = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-
         public MainWindow()
         {
             InitializeComponent();
-            BTep.ToolTip = "Эпсилон может быть только один";
+            BtnEpsilon.ToolTip = "Эпсилон может быть только один";
         }
 
         private void AddPr_Click(object sender, RoutedEventArgs e)
@@ -31,7 +29,7 @@ namespace Analizer
                 SecondRule.Clear();
             }
         }
-
+        
         private void Analizer_Click(object sender, RoutedEventArgs e)
         {
             int result = 0;   // результат конечный и 4 промежуточных
@@ -41,237 +39,58 @@ namespace Analizer
                 return;
             }
 
-            CountRules.Content = ListRules.Items.Count; // массив строк для правил
-            for (int index = 0; index < ListRules.Items.Count; index++) // перебираем все правила до конца
+            foreach (string stringRule in ListRules.Items)
             {
-                var aGrammar = false;
-                var contextFreeGrammar = false;
-                var contextSensitiveGrammar = false;
-                string rule = Convert.ToString(ListRules.Items[index]);
-                int separator = rule.IndexOf('-'); // выделяем первую часть правила и вторую
-                string firstUnitRule = rule.Substring(0, separator);
-                string secontUnitRule = rule.Substring(separator + 2);
-                int leftContext = 0; // обнуляем левый контекст
-                int rightContext = 0; // обнуляем правый контекст
-                int indexLeftContext = 0; // индексы правого и левого контекстов
-                int indexRightContext = 0; // начинаем с 1 если i=0, так как при умножении выйдем за пределы массива
-                int legthFirstUnitRule = firstUnitRule.Length; // размер 1 части правила
-                int legthSecondUnitRule = secontUnitRule.Length; // размер 2 части правила
-                bool theLeftEndOfContext = false; // индекс конца контекста слева
-                bool theRihtEndOfContext = false; // индекс конца контекса справа
-                bool errorRule = false;
                 /* проверка на А-грамматику */
-                int resultAGrammar;   //результат конечный и 4 промежуточных
-                if (legthFirstUnitRule == 1 || legthSecondUnitRule <= 2) // A грамматика
+                (string firstUnitRule, string secontUnitRule) rule = (stringRule.Substring(0, stringRule.IndexOf('-')), stringRule.Substring(stringRule.IndexOf('-') + 2)); //разделяем правило на левое и правое
+                var aGrammar = CheckAGrammar(rule);
+                if (aGrammar)
                 {
-                    if (legthFirstUnitRule == 1 && legthSecondUnitRule == 2)
+                    var resultAGrammar = 1;   //результат конечный и 4 промежуточных
+                    if (resultAGrammar > result)
                     {
-                        //Regex regex = new Regex("([a-z][A-Z])||([A-Z][a-z])||([a-z])"); //проверить на эпсилон
-                        //MatchCollection match = regex.Matches(secontUnitRule);
-                        bool firstSymbol = false;
-                        bool secontSymbolFirstNonTerm = false;
-                        bool secontSymbolFirstTerm = false;
-                        for (int y = 0; y < 26; y++)
-                        {
-                            if (firstUnitRule.IndexOf(Convert.ToChar(_all[y])) != -1)
-                            {
-                                //проверить на следующую грамматику
-                            }
-                            char symbol = Convert.ToChar(_all[y]);
-                            char symbolGrammar = Convert.ToChar(firstUnitRule[0]);
-
-                            if (symbol == symbolGrammar)
-                            {
-                                firstSymbol = true;
-                            }
-
-                            if (symbol == Convert.ToChar(secontUnitRule[0]))
-                            {
-                                secontSymbolFirstNonTerm = true;
-                            }
-
-                            if (symbol == Convert.ToChar(secontUnitRule[1]))
-                            {
-                                secontSymbolFirstTerm = true;
-                            }
-                        }
-
-                        if (firstSymbol && secontSymbolFirstNonTerm && secontSymbolFirstTerm == false || firstSymbol && secontSymbolFirstNonTerm == false && secontSymbolFirstTerm)
-                        {
-                            aGrammar = true;
-                            resultAGrammar = 1;
-                            if (resultAGrammar > result)
-                            {
-                                result = resultAGrammar;
-                            }
-                        }
+                        result = resultAGrammar;
                     }
 
-                    if (legthFirstUnitRule == 1 && legthSecondUnitRule == 1)
-                    {
-                        for (int y = 0; y < 26; y++)
-                        {
-                            char symbol = Convert.ToChar(_all[y]);
-                            char symbolGrammar = Convert.ToChar(firstUnitRule[0]);
-
-                            if (symbol == symbolGrammar)
-                            {
-                                aGrammar = true;
-                                resultAGrammar = 1;
-                                if (resultAGrammar > result)
-                                {
-                                    result = resultAGrammar;
-                                }
-                                break;
-                            }
-
-                        }
-
-                    }
+                    // если правило есть A-гр то нет смысла проверять дальше идем к следующему правилу 
+                    // TODO добавить вывод результата в конце итерации
+                    continue;
                 }
-                
+
                 /*
                 * проверка на КС-грамматику
                 */
-                if (aGrammar == false)
-                //если не А грамматика то проверяем дальше
+                var contextFreeGrammar = CheckContextFreeGrammar(rule);
+                if (contextFreeGrammar)
                 {
-                    if (legthFirstUnitRule == 0 && legthSecondUnitRule >= 1)
+                    var resultContextFreeGrammar = 2;   //результат конечный и 4 промежуточных
+                    if (resultContextFreeGrammar > result)
                     {
-                        for (int y = 0; y < 26; y++)
-                        {
-                            char symbol = Convert.ToChar(_all[y]);
-                            char symbolGrammar = Convert.ToChar(firstUnitRule[0]);
-
-                            if (symbol == symbolGrammar)
-                            {
-                                contextFreeGrammar = true;
-                                var resultContextFreeGrammar = 2;   //результат конечный и 4 промежуточных
-                                if (resultContextFreeGrammar > result)
-                                {
-                                    result = resultContextFreeGrammar;
-                                }
-                                break;
-                            }
-                        }
-
-
+                        result = resultContextFreeGrammar;
                     }
+
+                    continue;
                 }
+
                 /*
-                    * проверка на КЗ-грамматику
-                    */
-                if (contextFreeGrammar == false && aGrammar == false)
-                //если не А- и не КС-грамматики, то проверяем на КЗ-грамматику
+                * проверка на КЗ-грамматику
+                */
+                if (rule.firstUnitRule == rule.secontUnitRule)
                 {
-                    index++;
-                    var k = 0;
-                    /*
-                        * проверка на одиннаковые 1 и 2 части
-                        */
-                    if (legthFirstUnitRule == legthSecondUnitRule)
+                    // выдавать ошибку
+                    MessageBox.Show("1 часть правил и 2 одинаковы!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+                var contextSensitiveGrammar = CheckContextSensitiveGrammar(rule);
+                if (contextSensitiveGrammar)
+                {
+                    var resultContextSensitiveGrammar = 3; //результат конечный и 4 промежуточных
+                    if (resultContextSensitiveGrammar > result)
                     {
-                        for (int i = 0; i <= legthFirstUnitRule; i++)
-                        {
-                            if (k == legthFirstUnitRule)
-                            {
-                                errorRule = true;
-                                break;
-                            }
-                            if (firstUnitRule[k] == secontUnitRule[k])
-                            {
-                                k++;
-                            }
-                        }
-                    }
-
-                    k = 0;
-
-                    if (errorRule)
-                    {
-                        MessageBox.Show("1 часть правил и 2 одинаковы!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    }
-
-                    if (legthSecondUnitRule >= legthFirstUnitRule - 1 && errorRule == false)
-                    // если размер 2 части правила больше размера 1 части правила и нет одиннаковой 1 и 2 части
-                    {
-                        while (theLeftEndOfContext == false) // нахождение левого контекста
-                        {
-                            if (k > legthFirstUnitRule || k > legthSecondUnitRule)
-                            {
-                                indexLeftContext = k - 1;
-                                break;
-                            }
-                            if (firstUnitRule[k] == secontUnitRule[k])
-                            {
-                                leftContext += 1;
-                                // накапливаем индекс левого контекста
-                                k++;
-                            }
-                            else
-                            {
-                                indexLeftContext = k;
-                                theLeftEndOfContext = true;
-                            }
-                        }
-
-                        --k;
-                        int index1 = legthFirstUnitRule;  // начинаем с конца каждого правила
-                        int index2 = legthSecondUnitRule;
-                        while (theRihtEndOfContext == false) // нахождение правого контекста
-                        {
-                            if (index1 < 0 || index2 < 0)
-                            {
-                                index1 += 1;
-                                break;
-                            }
-
-                            if (firstUnitRule[index1] == secontUnitRule[index2])
-                            {
-                                rightContext += 1;
-                                index1 -= 1;
-                                index2 -= 1;
-                            }
-                            else
-                            {
-                                indexRightContext = index2;
-                                theRihtEndOfContext = true;
-                            }
-                        }
-
-                        index--;
-                        legthFirstUnitRule++;
-                        legthSecondUnitRule++;
-                        if (legthFirstUnitRule < legthSecondUnitRule)
-                        {
-                            if (indexLeftContext > indexRightContext)
-                            {
-                                index1 += 1;
-                            }
-                        }
-
-                        if (leftContext > 0 || rightContext > 0) // когда контекст находится и справа и слева
-                        {
-                            for (int y = 0; y < 26; y++)
-                            {
-                                char symbol = Convert.ToChar(_all[y]);
-                                char symbolGrammar = Convert.ToChar(firstUnitRule[index1]); 
-                                if (symbol == symbolGrammar)
-                                {
-                                    contextSensitiveGrammar = true;
-                                    var resultContextSensitiveGrammar = 3;   //результат конечный и 4 промежуточных
-                                    if (resultContextSensitiveGrammar > result)
-                                    {
-                                        result = resultContextSensitiveGrammar;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
+                        result = resultContextSensitiveGrammar;
                     }
                 }
-                    
-                if (contextSensitiveGrammar == false && contextFreeGrammar == false && aGrammar == false)
+                else
                 {
                     var resultZeroGrammar = 4;   //результат конечный и 4 промежуточных
                     if (resultZeroGrammar > result)
@@ -302,6 +121,7 @@ namespace Analizer
             {
                 label14.Content = "0-гр.";
             }
+            CountRules.Content = ListRules.Items.Count;
         }
 
         private void button_clearAll_Click(object sender, RoutedEventArgs e)
@@ -334,7 +154,7 @@ namespace Analizer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SecondRule.Text = Convert.ToString(BTep.Content);
+            SecondRule.Text = Convert.ToString(BtnEpsilon.Content);
         }
 
         private void Text_input(object sender, TextCompositionEventArgs e)
@@ -348,9 +168,10 @@ namespace Analizer
 
             e.Handled = "12345667890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm".IndexOf(e.Text, StringComparison.Ordinal) < 0;
         }
+
         private void AddSimbol(object sender, RoutedEventArgs e)
         {
-            char epsilon = Convert.ToChar(BTep.Content);
+            char epsilon = Convert.ToChar(BtnEpsilon.Content);
             string pEpsilon = Convert.ToString(SecondRule.Text);
             int leng = pEpsilon.Length;
             bool bEpsilon = false;
@@ -375,15 +196,100 @@ namespace Analizer
             }
         }
 
+        private bool CheckAGrammar((string firstUnitRule, string secontUnitRule) rule)
+        {
+            if (rule.firstUnitRule.Length == 1 || rule.secontUnitRule.Length <= 2)
+            {
+                // размер левой части в А гр всегда равен 1, а второй может быть как 1 так и 2
+                if (new Regex(@"[A-Z]").Match(rule.firstUnitRule).Length == 0)
+                {
+                    // левая часть правила должна быть нетерминальный символом
+                    return false;
+                }
 
+                if (rule.secontUnitRule.Length == 1)
+                {
+                    if (new Regex(@"[A-Z]").Match(rule.secontUnitRule).Length != 0)
+                    {
+                        // если в правой части один символ то он должен быть терминалом
+                        return false;
+                    }
+
+                    return true; // если размер подходит и это терминал, то это A-гр
+                }
+
+                if (new Regex(@"[A-Z]").Matches(rule.secontUnitRule).Count == 1)
+                {
+                    //обязательно должен быть нетерминал если размер правой части равен 2
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool CheckContextFreeGrammar((string firstUnitRule, string secontUnitRule) rule)
+        {
+            if (rule.firstUnitRule.Length == 1 && new Regex(@"[A-Z]").Match(rule.firstUnitRule).Length == 1)
+            {
+                // правая часть всегда равна 1, и там обязательно должен быть нетерминал
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckContextSensitiveGrammar((string firstUnitRule, string secontUnitRule) rule)
+        {
+            if (rule.firstUnitRule.Length > rule.secontUnitRule.Length) // правая часть должна быть больше или равна левой
+            {
+                return false;
+            }
+            
+            rule = DeleteContext(rule); // обрезаем контекст у правила
+
+            return CheckContextFreeGrammar(rule); // должно выглядеть как правило КС граммматики
+        }
+
+        /// <summary>
+        /// Удалить контекст из правила
+        /// </summary>
+        /// <param name="rule">Правило</param>
+        /// <returns>Правило</returns>
+        private (string firstUnitRule, string secontUnitRule) DeleteContext((string firstUnitRule, string secontUnitRule) rule)
+        {
+            for (int i = 0; ; i++) // обрезаем левый контекст
+            {
+                if (rule.firstUnitRule[i] != rule.secontUnitRule[i])
+                {
+                    break;
+                }
+
+                rule.firstUnitRule = rule.firstUnitRule.Substring(1); // обрезать первый символ
+                rule.secontUnitRule = rule.secontUnitRule.Substring(1);
+            }
+
+            for (int i = rule.firstUnitRule.Length - 1, k = rule.secontUnitRule.Length - 1; ; i--, k--)
+            {
+                if (rule.firstUnitRule[i] != rule.secontUnitRule[k])
+                {
+                    break;
+                }
+
+                rule.firstUnitRule = rule.firstUnitRule.Substring(0, rule.firstUnitRule.Length - 1); // обрезать последний символ
+                rule.secontUnitRule = rule.secontUnitRule.Substring(0, rule.secontUnitRule.Length - 1);
+            }
+
+            return rule;
+        }
 
         private void AddSimbol2(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
             if (button != null)
             {
-                String symbol = Convert.ToString(button.Content);
-                String text = FirstRule.Text;
+                string symbol = Convert.ToString(button.Content);
+                string text = FirstRule.Text;
                 text += symbol;
                 FirstRule.Text = text;
             }
---
+++ Analizer/Models/GrammarType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Analizer.Models
+{
+    public enum GrammarType
+    {
+        [Display(Name = "А-грамматика")]
+        A,
+        [Display(Name = "Контекстно-свободная грамматика")]
+        ContextFree,
+        [Display(Name = "Контекстно-зависимая грамматика")]
+        ContextSensitive,
+        [Display(Name = "Нулевая грамматика")]
+        Zero
+    }
+}
